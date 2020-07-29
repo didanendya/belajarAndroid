dea08d9008774311f0166fffe8828d2fbb32e224
Matahari Bhakti Nendya <dida.nendya@gmail.com>
2020-07-29 15:23:18 +0700

    view and views
    views and views module

---
+++ MyViewAndViews/app/src/main/java/com/didanendya/ajaran/myviewandviews/MainActivity.java
@@ -0,0 +1,14 @@
+package com.didanendya.ajaran.myviewandviews;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
